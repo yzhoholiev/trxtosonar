c0793c3cc37d61cd0bf3d42165b4004ecff7d342
rmcc13 <6742231+rmcc13@users.noreply.github.com>
2019-06-20 23:22:47 -0400

    build: replace commandline utils library
    replace Microsoft.Extensions.CommandLineUtils with
    McMaster.Extensions.CommandLineUtils

--- TrxToSonar/Program.cs
+++ TrxToSonar/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using Microsoft.Extensions.CommandLineUtils;
-using Microsoft.Extensions.Configuration;
+﻿using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using TrxToSonar.Model.Sonar;
-using TrxToSonar.Model.Trx;
 
 namespace TrxToSonar
 {
@@ -17,7 +12,7 @@ namespace TrxToSonar
             ConfigureServices(serviceCollection);
 
             var serviceProvider = serviceCollection.BuildServiceProvider();
-            
+
             var app = new CommandLineApplication
             {
                 Name = "Trx To Sonar",
@@ -25,28 +20,30 @@ namespace TrxToSonar
             };
 
             app.HelpOption("-?|-h|--help");
- 
-            var solutionDirectoryOption = app.Option("-d","Solution Directory to parse.",CommandOptionType.SingleValue);
+
+            var solutionDirectoryOption = app.Option("-d", "Solution Directory to parse.", CommandOptionType.SingleValue);
             var outputOption = app.Option("-o", "Output filename.", CommandOptionType.SingleValue);
             var absolutePathOption = app.Option("-a|--absolute", "Use Absolute Path", CommandOptionType.NoValue);
- 
-            app.OnExecute(() => {
+
+            app.OnExecute(() =>
+            {
                 if (solutionDirectoryOption.HasValue() && outputOption.HasValue())
                 {
                     var converter = serviceProvider.GetService<IConverter>();
                     var sonarDocument = converter.Parse(solutionDirectoryOption.Value(), absolutePathOption.HasValue());
                     converter.Save(sonarDocument, outputOption.Value());
                 }
-                else {
+                else
+                {
                     app.ShowHint();
                 }
- 
+
                 return 0;
             });
- 
+
             app.Execute(args);
         }
-        
+
         private static void ConfigureServices(IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton(new LoggerFactory().AddConsole());
