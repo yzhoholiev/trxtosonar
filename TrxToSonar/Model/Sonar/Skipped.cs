6ea2a5b1c44e234b5a5da06ba6d62d4d133edbc6
Yurii Zhoholiev <yurii.zhoholiev@gmail.com>
2020-08-04 17:07:22 +0300

    Handle skipped tests.

--- TrxToSonar/Model/Sonar/Skipped.cs
+++ TrxToSonar/Model/Sonar/Skipped.cs
@@ -5,7 +5,7 @@ namespace TrxToSonar.Model.Sonar
     public class Skipped
     {
         [XmlAttribute(AttributeName = "message")]
-        public string Message { get; set; }
+        public string Message { get; set; } = "Skipped";
 
         [XmlText]
         public string Value { get; set; }
