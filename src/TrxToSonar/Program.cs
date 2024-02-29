aa94eefc76bba8907cf25f273de27d84227dea39
Yurii Zhoholiev <yurii.zhoholiev@gmail.com>
2024-02-29 17:44:36 +0200

    Use Serilog

--- src/TrxToSonar/Program.cs
+++ src/TrxToSonar/Program.cs
@@ -1,6 +1,13 @@
-﻿using McMaster.Extensions.CommandLineUtils;
+﻿using System.Globalization;
+using McMaster.Extensions.CommandLineUtils;
+using Serilog;
 using TrxToSonar;
 using TrxToSonar.Model.Sonar;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Async(config => config.Console(formatProvider: CultureInfo.InvariantCulture, applyThemeToRedirectedOutput: true))
+    .CreateLogger();
 
 using var app = new CommandLineApplication();
 
@@ -16,26 +23,24 @@ CommandOption absolutePathOption = app.Option("-a|--absolute", "Use Absolute Pat
 app.OnExecute(
     () =>
     {
-        var serviceCollection = new ServiceCollection();
-        ConfigureServices(serviceCollection);
-
-        using ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
-
-        if (solutionDirectoryOption.HasValue() && outputOption.HasValue())
-        {
-            IConverter converter = serviceProvider.GetRequiredService<IConverter>();
-            SonarDocument? sonarDocument = converter.Parse(solutionDirectoryOption.Value(), absolutePathOption.HasValue());
-            if (sonarDocument is null)
-            {
-                return 1;
-            }
-
-            converter.Save(sonarDocument, outputOption.Value()!);
-        }
-        else
+        if (!solutionDirectoryOption.HasValue() || !outputOption.HasValue())
         {
             app.ShowHint();
+            return 0;
         }
+
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        using ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+        IConverter converter = serviceProvider.GetRequiredService<IConverter>();
+        SonarDocument? sonarDocument = converter.Parse(solutionDirectoryOption.Value(), absolutePathOption.HasValue());
+        if (sonarDocument is null)
+        {
+            return 1;
+        }
+
+        converter.Save(sonarDocument, outputOption.Value()!);
 
         return 0;
     });
@@ -44,6 +49,6 @@ return app.Execute(args);
 
 static void ConfigureServices(IServiceCollection serviceCollection)
 {
-    serviceCollection.AddLogging(builder => builder.AddConsole());
+    serviceCollection.AddLogging(builder => builder.AddSerilog(dispose: true));
     serviceCollection.AddSingleton<IConverter, Converter>();
 }
